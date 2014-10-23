a233fa9c63a21383dec113aa867460959cf6f091
alec <azadikian1@hwemail.com>
2014-10-23 12:15:58 -0700

    Initial Commit

---
+++ HeartsServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HeartsServer
+//
+//  Created by Alec Zadikian on 10/23/14.
+//  Copyright (c) 2014 AlecZ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

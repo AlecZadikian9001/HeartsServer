63dc429e60eebc4ed15fe46cdbbe4cfb6398c84d
Alec Zadikian <alecz@berkeley.edu>
2014-12-18 16:48:01 -0800

    commit

--- HeartsAI/HeartsAI/main.c
+++ HeartsAI/HeartsAI/main.c
@@ -58,20 +58,125 @@ typedef enum{
 #define MAX_PLAYERS (6)
 #define MAX_CARDS_PER_PLAYER (DECK_SIZE/MAX_PLAYERS)
 typedef int card; // card value = rank*RANK_PRIME + suit*SUIT_PRIME
-// Communcation constants ===vv
 #define MAX_NAME_LENGTH (16)
 #define SEND_BUFFER_LEN (8 + MAX_CARDS_PER_PLAYER*3)
 #define RECV_BUFFER_LEN (4 + MAX_NAME_LENGTH)
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    int fifo = mkfifo("/tmp/RandomBot", S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
-    int fd = open("/tmp/RandomBot", O_WRONLY);
+    // Generate random name...v
     char name[8];
-    for (int i = 0; i<8; i++){
-        name[i] = 'a' + rand()%4;
+    srand (time(NULL));
+    for (int i = 0; i<7; i++){
+        name[i] = 'a' + rand()%20;
     }
+    name[7] = '\0';
     printf("Starting RandomBot with name: %s\n", name);
+    //...^
+    // Create I/O named pipes, and open for reading and writing...v
+    int fifoOut = mkfifo("/tmp/RandomBotOut", S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
+    int out = open("/tmp/RandomBotOut", O_RDONLY);
+    int fifoIn = mkfifo("/tmp/RandomBotIn", S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
+    int in = open("/tmp/RandomBot", O_WRONLY);
+    // ...^
+    
+    /*
+     Reference of message headers:{
+     _Message Header_               _Meaning_				_Action_
+     "@"							Send name				ASAP send the name of your AI.
+     ":"							Round starting			Read in your hand, set up your AI, etc.
+     "["							Asking for play			ASAP send the index of the card you want to play.
+     "]"							Card played				Take note that that player played that card.
+     ";"							Testing ended			Shut down your AI.
+     }
+     */
+    
+    // General loop/communication variables...v
+    unsigned char sendBuffer[SEND_BUFFER_LEN];
+    unsigned char recvBuffer[RECV_BUFFER_LEN];
+    size_t recvRet;
+    size_t sendRet;
+    int scanIndex;
+    int cardIndex;
+    bool endScan;
+    char currentChar;
+    bool running = true;
+    //...^
+    // Card variables...v
+    card hand[MAX_CARDS_PER_PLAYER];
+    memset(hand, NULL_CARD, MAX_CARDS_PER_PLAYER);
+    int playerID; // This AI's player ID
+    int numPlayers; // Number of players in game
+    int turn; // Whose turn is it?
+    int nextMove = -1; // -1 means no next move.
+    //...^
+    while (running){
+        recvRet = cTalkRecv(in, recvBuffer, RECV_BUFFER_LEN);
+        switch (recvBuffer[0]){
+            case ']':{ // Card Played
+                // Format: "]player,card"
+                //TODO check to see what suit we need to play
+                /*
+                 YOUR CODE HERE
+                 */
+                break;
+            }
+            case '[':{ // Asking for Play
+                // TODO just play a random, valid card
+                break;
+            }
+            case ':':{ // New Round
+                // Format: ":numPlayers,playerID,turn,card1,card2,card3,...cardN"
+                numPlayers = atoi(recvBuffer+1);
+                scanIndex=1;
+                while (recvBuffer[scanIndex]!=',') scanIndex++;
+                scanIndex++;
+                playerID = atoi(recvBuffer+scanIndex);
+                while (recvBuffer[scanIndex]!=',') scanIndex++;
+                scanIndex++;
+                turn = atoi(recvBuffer+scanIndex);
+                cardIndex = 0;
+                endScan = false;
+                while (true){
+                    while (recvBuffer[scanIndex]!=','){
+                        currentChar = recvBuffer[scanIndex];
+                        if (currentChar !='\0' && currentChar != ',' && (currentChar < '0' || currentChar > '9')){ // Error-checking
+                            printf("***ERROR: RECEIVED INVALID MESSAGE FROM SERVER\n");
+                            exit(1);
+                        }
+                        if (currentChar=='\0'){
+                            endScan = true;
+                            break;
+                        }
+                        scanIndex++;
+                    }
+                    if (endScan) break;
+                    scanIndex++;
+                    hand[cardIndex] = atoi(recvBuffer+scanIndex);
+                    cardIndex++;
+                }
+                
+                // Start calculating the first move right now...v
+                for (int i = 0; i<MAX_CARDS_PER_PLAYER; i++){ // Just see if we have the Two of Clubs.
+                    if (hand[i]==makeCard(suit_clubs, rank_2)){
+                        nextMove = i;
+                        break;
+                    }
+                }
+                //...^
+                
+                break;
+            }
+            case '@':{ // Asking for Name
+                sendRet = cTalkSend(out, name, strlen(name)+1); // Send the name.
+                break;
+            }
+            case ';':{ // End of Tests
+                printf("Tests are over. Exiting.\n");
+                exit(0);
+                break;
+            }
+        }
+    }
     
     return 0;
 }
